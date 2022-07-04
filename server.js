dbb39870a6802d1b14ba2469ce1f19ad05403164
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-07-04 20:15:03 -0300

    Add middleware and starting the routes

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const mongoose = require('mongoose');
 var Produto = require('./app/models/product');
 
 // connect mongodb
-mongoose.connect('mongodb+srv://admin:<KsaS4UXtQyLZQNHK>@cluster0.xmvpd.mongodb.net/?retryWrites=true&w=majority');
+/*mongoose.connect('mongodb+srv://admin:<KsaS4UXtQyLZQNHK>@cluster0.xmvpd.mongodb.net/?retryWrites=true&w=majority', {
+    useMongoClient: true
+}); */
 
 // app vai utilizar bodyParser
 // app = bodyParser
@@ -21,9 +23,19 @@ const port = process.env.port || 8000;
 // criando uma rota utilizando o express
 // create the routes by express
 const router = express.Router();
+//middleware
+router.use(function(req, res, next) {
+    console.log('teste');
+    next(); // Caso tenha problema, o next irá levar a outra rota
+});
+
 router.get('/', function(req, res) {
     res.json({ message: 'Olá, tudo bem? Seja bem vindo!'})
 });
+
+//APIS:
+
+
 
 // criando um padrão para as rotas
 // create the standard for the routes
